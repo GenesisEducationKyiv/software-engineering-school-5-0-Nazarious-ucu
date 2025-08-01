fdcd2a2515c8fb20c21c84a59644d100d5f32c6f
Nazarious-ucu <parnosov.pn@ucu.edu.ua>
2025-08-01 05:45:20 +0300

    feat(notification): add logging to emailer

--- notification/internal/emailer/emailer.go
+++ notification/internal/emailer/emailer.go
@@ -1,43 +1,72 @@
 package emailer
 
 import (
-	"log"
 	"net/smtp"
+	"time"
 
 	"github.com/Nazarious-ucu/weather-subscription-api/notification/internal/config"
+	"github.com/Nazarious-ucu/weather-subscription-api/notification/internal/metrics"
+	"github.com/rs/zerolog"
 )
 
+// SMTPService wraps smtp.SendMail with structured logging and metrics.
 type SMTPService struct {
 	user     string
 	host     string
 	port     string
 	password string
 	From     string
-	logger   *log.Logger
+	logger   zerolog.Logger
+	m        metrics.Metrics
 }
 
-func NewSMTPService(cfg *config.Config, logger *log.Logger) *SMTPService {
-	svc := &SMTPService{
+// NewSMTPService creates an SMTPService with a scoped logger and metrics.
+func NewSMTPService(cfg *config.Config, logger zerolog.Logger, m metrics.Metrics) *SMTPService {
+	logger = logger.With().Str("component", "SMTPService").Logger()
+	return &SMTPService{
 		user:     cfg.Email.User,
 		host:     cfg.Email.Host,
 		port:     cfg.Email.Port,
 		password: cfg.Email.Password,
 		From:     cfg.Email.From,
 		logger:   logger,
+		m:        m,
 	}
-
-	return svc
 }
 
+// Send sends an email and records logs + metrics.
 func (e *SMTPService) Send(to, subject, additionalHeaders, body string) error {
-	auth := smtp.PlainAuth("", e.user, e.password, e.host)
+	start := time.Now()
+	e.logger.Debug().
+		Str("to", to).
+		Str("subject", subject).
+		Msg("sending email")
 
+	auth := smtp.PlainAuth("", e.user, e.password, e.host)
 	msg := "From: " + e.From + "\n" +
 		"To: " + to + "\n" +
 		"Subject: " + subject + "\n" +
 		additionalHeaders + "\n\n" +
 		body
-
 	addr := e.host + ":" + e.port
-	return smtp.SendMail(addr, auth, e.user, []string{to}, []byte(msg))
+
+	err := smtp.SendMail(addr, auth, e.user, []string{to}, []byte(msg))
+	duration := time.Since(start)
+
+	if err != nil {
+		e.logger.Error().
+			Err(err).
+			Str("to", to).
+			Str("subject", subject).
+			Dur("duration", duration).
+			Msg("email send failed")
+		return err
+	}
+
+	e.logger.Info().
+		Str("to", to).
+		Str("subject", subject).
+		Dur("duration", duration).
+		Msg("email sent successfully")
+	return nil
 }
